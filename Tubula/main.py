cc257dec14a64c42415123a2376f8b32db610789
anatoliynt <anatoliynt@gmail.com>
2023-04-11 16:01:53 +0500

    create PDF to Excel on Tabula

--- Tubula/main.py
+++ Tubula/main.py
@@ -1,2 +1,15 @@
-import tubula
+import tabula
 import pandas as pd
+
+pdf_one_file = "E:\Python\Project\PDF_to_Excel\original_files" \
+               "\\bad_file.pdf"
+
+tabula_pdf = tabula.read_pdf(pdf_one_file, pages='all')
+
+csv_tabula = tabula.convert_into("E:\Python\Project\PDF_to_Excel"
+                                 "\original_files\\bad_file.pdf", "out.csv",
+                                 output_format="csv", pages='all', lattice=":")
+
+new_pad = pd.DataFrame(csv_tabula)
+
+print(new_pad)
